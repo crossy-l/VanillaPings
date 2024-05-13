ff4800b92131b188deb0aff335d7d34a9869bf38
crossyl2001 <crossfield2006@gmail.com>
2024-05-13 12:02:29 +0200

    feat: Added 9 new commands to manage settings
    (cherry picked from commit 1c3df4dc66cb8e65ec26e0afd1b466081e5f1054)

---
+++ src/main/java/com/vanillapings/commands/ItemCommand.java
@@ -0,0 +1,25 @@
+package com.vanillapings.commands;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+import com.vanillapings.VanillaPings;
+import com.vanillapings.translation.Translations;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.server.command.ServerCommandSource;
+public class ItemCommand {
+    public static int setItem(CommandContext<ServerCommandSource> ctx, Item item) {
+        VanillaPingsCommands.sendCommandFeedBack(Translations.ITEM.constructMessage(Registries.ITEM.getId(item).toString(), VanillaPings.SETTINGS.setPingItem(item)), ctx.getSource());
+        return Command.SINGLE_SUCCESS;
+    }
+
+    public static int setItemCountRange(CommandContext<ServerCommandSource> ctx, double value) {
+        VanillaPingsCommands.sendCommandFeedBack(Translations.ITEM_COUNT_RANGE.constructMessage(value, VanillaPings.SETTINGS.setItemCountRange(value)), ctx.getSource());
+        return Command.SINGLE_SUCCESS;
+    }
+
+    public static int setItemCount(CommandContext<ServerCommandSource> ctx, boolean value) {
+        VanillaPingsCommands.sendCommandFeedBack(Translations.ITEM_COUNT_ENABLED.constructMessage(value, VanillaPings.SETTINGS.setPingItemCount(value)), ctx.getSource());
+        return Command.SINGLE_SUCCESS;
+    }
+}
--- src/main/java/com/vanillapings/commands/SoundCommand.java
+++ src/main/java/com/vanillapings/commands/SoundCommand.java
@@ -8,7 +8,7 @@ import net.minecraft.server.command.ServerCommandSource;
 
 public class SoundCommand {
     public static int setSound(CommandContext<ServerCommandSource> ctx, boolean value) {
-        VanillaPingsCommands.sendCommandFeedBack(Translations.SOUND_ENABLED.constructMessage(value, !VanillaPings.SETTINGS.setPlaySound(value)), ctx.getSource());
+        VanillaPingsCommands.sendCommandFeedBack(Translations.SOUND_ENABLED.constructMessage(value, VanillaPings.SETTINGS.setPlaySound(value)), ctx.getSource());
         return Command.SINGLE_SUCCESS;
     }
 }
